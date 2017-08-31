8cbaae2f529f30c1892d3599db6c9aa8e61baf16
Dejan Budimir <dejbug@users.noreply.github.com>
2017-08-31 02:25:42 +0200

    list every file on the server

--- appveyor.bat
+++ appveyor.bat
@@ -1,53 +1,12 @@
-REM IF NOT EXIST build MKDIR build
-REM python src\print_file_system_info.py > build\print_file_system_info.out.txt
-cinst winflexbison
-DIR /S /B C:\ProgramData\chocolatey\lib\winflexbison
-
-EXIT
-
-@ECHO off
-
-ECHO -- [ running ] "stage: prepare"
-
-ECHO -- setting MINGW paths
-SET MINGW_BIN=C:\mingw-w64\i686-5.3.0-posix-dwarf-rt_v4-rev0\mingw32\bin
-SET PATH=%MINGW_BIN%;%PATH%
-
-ECHO -- renaming mingw32-make.exe in-place
-PUSHD %MINGW_BIN%
-RENAME mingw32-make.exe make.exe
-POPD
-
-ECHO -- [ running ] "stage: build"
-
-ECHO -- selecting build target
-IF "%APPVEYOR_REPO_TAG%"=="true" GOTO build_release
-IF "%APPVEYOR_REPO_TAG%"=="false" GOTO build_debug
-ECHO [ ERROR ] invalid build type
+GOTO TEST_1
 GOTO :EOF
 
-:build_debug
+:TEST_1
+IF NOT EXIST build MKDIR build
+python src\print_file_system_info.py > build\print_file_system_info.out.txt
+GOTO :EOF
 
-ECHO -- building for debug
-make TARGET=debug WINFLAGS=
-
-GOTO run_test
-
-:build_release
-
-ECHO -- building for release
-make TARGET=release WINFLAGS=
-
-ECHO -- writing release.txt
-ECHO %APPVEYOR_PROJECT_NAME%-%APPVEYOR_REPO_TAG_NAME%.zip > build\release.txt
-
-ECHO -- packing output to deploy
-IF NOT EXIST deploy MKDIR deploy
-7z a deploy\%APPVEYOR_PROJECT_NAME%-%APPVEYOR_REPO_TAG_NAME%.zip build\*
-
-DIR deploy
-
-:run_test
-
-
+:TEST_2
+cinst winflexbison
+DIR /S /B C:\ProgramData\chocolatey\lib\winflexbison
 GOTO :EOF
--- src/print_file_system_info.py
+++ src/print_file_system_info.py
@@ -1,10 +1,12 @@
 import os
 import re
 
+flag_only_exe = False
+
 re_is_exe = re.compile(r'(?i).*\.exe')
 
 for t,dd,nn in os.walk(u'c:\\'):
 	nn = sorted(nn, key=lambda n: n.lower())
-	nn = (n for n in nn if re_is_exe.match(n))
-	for n in nn:
-		print os.path.join(t, n)
+	if flag_only_exe: nn = (n for n in nn if re_is_exe.match(n))
+	pp = (os.path.join(t, n) for n in nn)
+	for p in pp: print p
