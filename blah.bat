b4f5a9bddcbe756e781bb431ab038ad3d07cf817
Dejan Budimir <dejbug@users.noreply.github.com>
2018-07-22 17:47:08 +0200

    -

--- blah.bat
+++ blah.bat
@@ -1,3 +1,9 @@
 @echo off
 for /f "delims=" %%a in ('python appveyor_mingw.py') do set MINGW_DIR=%%a
-echo '%MINGW_DIR%'
+rem echo '%MINGW_DIR%'
+if "."=="%MINGW_DIR%" (
+	set ERRORLEVEL=1
+) else (
+	set ERRORLEVEL=0
+	echo %MINGW_DIR%
+)
